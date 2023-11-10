5cf7d6230ca53a87b0efa0db9c434ee1d0878eb0
Dawijak04 <23384549@studentmail.ul.ie>
2023-11-10 11:43:47 +0000

    Skin and bones

---
+++ GitCalculator.java
@@ -0,0 +1,37 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class GitCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Welcome to git calculator");
+		System.out.println("Input the desired operation");
+		System.out.println("Addition(A)/Subtraction(S)/Multiplication(M)/Division(D)");
+		String operation = scan.next();
+		System.out.println("Input first integer");
+		int number1 = scan.nextInt();
+		System.out.println("Input second integer");
+		int number2	= scan.nextInt();
+		
+		if (operation.equalsIgnoreCase("A")) {
+			
+		}
+		else if (operation.equalsIgnoreCase("S")) {
+			
+		}
+		else if (operation.equalsIgnoreCase("M")) {
+			
+		}
+		else if (operation.equalsIgnoreCase("D")) {
+			
+		}
+		else {
+			System.out.println("Unrecognized");
+		}
+		}
+
+}
